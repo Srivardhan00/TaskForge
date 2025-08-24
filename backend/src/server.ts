7f544a1edfbe5ac2c7bb8c45e669f741a4e88885
Srivardhan00 <srivardhanveeramalli@gmail.com>
2025-08-24 15:21:53 +0530

    setup Tailwind in frontend and Prisma with Postgres in backend

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,7 +1,7 @@
 import express from "express";
 import type { Request, Response } from "express";
 import cors from "cors";
-import "dotenv/config"; // cleaner than calling dotenv.config()
+import "dotenv/config";
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -16,3 +16,25 @@ app.get("/ping", (req: Request, res: Response) => {
 app.listen(PORT, () => {
   console.log(`🚀 Server running on http://localhost:${PORT}`);
 });
+
+import { log } from 'console'
+import { PrismaClient } from '../generated/prisma'
+
+const prisma = new PrismaClient()
+
+async function main() {
+  // ... you will write your Prisma Client queries here
+  console.log("Prisma Client is ready");
+  console.log(prisma);
+  
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect()
+  })
+  .catch(async (e) => {
+    console.error(e)
+    await prisma.$disconnect()
+    process.exit(1)
+  })
